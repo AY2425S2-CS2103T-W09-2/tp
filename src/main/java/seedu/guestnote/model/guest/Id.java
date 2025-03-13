2a0d0591a1dd862249c476ea9500f2dd49f593b8
salads-source <ron52086@gmail.com>
2025-03-13 08:55:28 +0800

    Add base class template for guestId

---
+++ src/main/java/seedu/guestnote/model/guest/Id.java
@@ -0,0 +1,11 @@
+package seedu.guestnote.model.guest;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.guestnote.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a guest ID in the guestnote book.
+ * Guarantees immutability
+ */
+public class Id {
+}
